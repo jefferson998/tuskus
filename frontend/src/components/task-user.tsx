4873753cb0f4dbfcb1982bb38a15620791d449b3
jefferson998 <pazjefferson11216@gmail.com>
2024-10-01 09:59:07 -0500

    test

--- frontend/src/components/task-user.tsx
+++ frontend/src/components/task-user.tsx
@@ -5,22 +5,24 @@ import axios from "axios";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTasks, addTask } from "../_store/tasksSlice"; // Asegúrate de que la ruta sea correcta
+import { fetchTasks, addTask } from "../_store/tasksSlice";
 import { MdClose } from "react-icons/md";
 import emptyStateSvg from "../../public/empty_state.svg";
 import { AppDispatch, RootState } from "../_store";
-import {URL_API} from '../config'
+import { URL_API } from '../config';
+import { BiEdit } from "react-icons/bi";
 
 function TaskUserPage() {
-  const [newTitle, setNewTitle] = useState(""); // Estado para el nombre de la nueva tarea
-  const [newDescription, setNewDescription] = useState(""); // Estado para la descripción
-  const [newProgreso, setNewProgreso] = useState(""); // Estado para el progreso
-  const [modalOpen, setModalOpen] = useState(false); // Estado para el modal
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
+  const [newProgreso, setNewProgreso] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false); // Nuevo estado para controlar si estamos editando
+  const [editTaskId, setEditTaskId] = useState<string | null>(null); // Guardar el ID de la tarea que se está editando
   const navigate = useNavigate();
   const [cookie] = useCookies();
   const dispatch: AppDispatch = useDispatch();
 
-  // Obtener tareas y estado de carga del store de Redux
   const { tasks = [], loading } = useSelector(
     (state: RootState) => state.tasks || {}
   );
@@ -33,25 +35,46 @@ function TaskUserPage() {
     }
   }, [cookie.token, navigate, dispatch]);
 
-  const handleAddTask = async (e: any) => {
-    e.preventDefault(); // Evitar que el formulario recargue la página
-
+  const handleAddOrUpdateTask = async (e: any) => {
+    e.preventDefault();
+    
     try {
-      await dispatch(
-        addTask({
-          title: newTitle,
-          description: newDescription,
-          progress: newProgreso,
-          token: cookie.token,
-        })
-      );
-      // Limpiar los campos después de agregar
+      if (isEditing && editTaskId) {
+        // Si estamos editando, realizar la actualización
+        await axios.put(
+          `${URL_API}/api/tasks/${editTaskId}`,
+          {
+            title: newTitle,
+            description: newDescription,
+            progress: newProgreso,
+          },
+          {
+            headers: {
+              token: cookie.token,
+            },
+          }
+        );
+      } else {
+        // Si no estamos editando, agregar una nueva tarea
+        await dispatch(
+          addTask({
+            title: newTitle,
+            description: newDescription,
+            progress: newProgreso,
+            token: cookie.token,
+          })
+        );
+      }
+
+      // Limpiar campos y cerrar el modal
       setNewTitle("");
       setNewDescription("");
       setNewProgreso("");
-      setModalOpen(false); // Cerrar el modal después de agregar
+      setModalOpen(false);
+      setIsEditing(false); // Reiniciar estado de edición
+      dispatch(fetchTasks(cookie.token)); // Recargar tareas
     } catch (error) {
-      console.error("Error adding task:", error);
+      console.error("Error saving task:", error);
     }
   };
 
@@ -59,14 +82,24 @@ function TaskUserPage() {
     try {
       await axios.delete(`${URL_API}/api/tasks/${taskId}`, {
         headers: {
-          token: cookie.token, // Enviar el token en el header
+          token: cookie.token,
         },
       });
-
-      // Filtrar la tarea eliminada de la lista
       dispatch(fetchTasks(cookie.token)); // Recargar tareas después de eliminar
     } catch (error) {
       console.error("Error deleting task:", error);
+    }
+  };
+
+  const editTask = (taskId: string) => {
+    const task:any = tasks.find((t: any) => t._id === taskId);
+    if (task) {
+      setNewTitle(task.title);
+      setNewDescription(task.description);
+      setNewProgreso(task.progress);
+      setEditTaskId(taskId); // Establecer la tarea que se va a editar
+      setIsEditing(true);
+      setModalOpen(true); // Abrir el modal
     }
   };
 
@@ -75,7 +108,10 @@ function TaskUserPage() {
       <Navbar />
       <div className="w-full h-full mt-12">
         <button
-          onClick={() => setModalOpen(true)}
+          onClick={() => {
+            setModalOpen(true);
+            setIsEditing(false); // No estamos editando al agregar una tarea
+          }}
           className="bg-primary-light/95 hover:bg-primary-light text-white/90 px-4 py-2 rounded-md transition"
         >
           Agregar Tarea
@@ -84,11 +120,9 @@ function TaskUserPage() {
         {modalOpen && (
           <div className="fixed inset-0 flex items-center justify-center bg-primary-dark bg-opacity-50 z-50">
             <div className="bg-primary-dark p-6 rounded-lg shadow-lg w-96">
-              {" "}
-              {/* Aumentar el ancho del modal */}
               <div className="flex justify-between -z-1">
                 <h2 className="text-xl font-bold mb-4 text-white/80">
-                  Agregar Nueva Tarea
+                  {isEditing ? "Editar Tarea" : "Agregar Nueva Tarea"}
                 </h2>
                 <button
                   onClick={() => setModalOpen(false)}
@@ -98,7 +132,7 @@ function TaskUserPage() {
                 </button>
               </div>
               <form
-                onSubmit={handleAddTask}
+                onSubmit={handleAddOrUpdateTask}
                 className="flex flex-col gap-2 w-full"
               >
                 <input
@@ -129,19 +163,9 @@ function TaskUserPage() {
                     onChange={(e) => setNewProgreso(e.target.value)}
                     className="w-full h-2 bg-primary-light/20 rounded-lg appearance-none cursor-pointer focus:outline-none"
                     style={{
-                      background: `linear-gradient(to right, #ffffff ${newProgreso}%, #4a4a4a ${newProgreso}%)`, // Progreso dinámico
+                      background: `linear-gradient(to right, #ffffff ${newProgreso}%, #4a4a4a ${newProgreso}%)`,
                     }}
                   />
-                  <style>
-                    {`
-      input[type="range"]::-webkit-slider-thumb {
-        @apply appearance-none w-4 h-4 rounded-full bg-white cursor-pointer; /* Color blanco para el thumb */
-      }
-      input[type="range"]::-moz-range-thumb {
-        @apply w-4 h-4 rounded-full bg-white cursor-pointer; /* Color blanco para el thumb */
-      }
-    `}
-                  </style>
                 </div>
 
                 <button
@@ -149,9 +173,13 @@ function TaskUserPage() {
                   className={`bg-primary-light mt-4 text-white px-4 py-2 rounded-md transition ${
                     loading ? "opacity-50 cursor-not-allowed" : ""
                   }`}
-                  disabled={loading} // Deshabilitar el botón si está cargando
+                  disabled={loading}
                 >
-                  {loading ? "Cargando..." : "Agregar"}
+                  {loading
+                    ? "Cargando..."
+                    : isEditing
+                    ? "Guardar Cambios"
+                    : "Agregar"}
                 </button>
               </form>
             </div>
@@ -159,7 +187,6 @@ function TaskUserPage() {
         )}
 
         <div className="w-full overflow-y-auto mt-4">
-          {/* Empty state */}
           {tasks.length === 0 && !loading ? (
             <div className="flex flex-col justify-center items-center text-white/70 py-10">
               <img
@@ -176,15 +203,25 @@ function TaskUserPage() {
                   key={task._id}
                   className="bg-primary-dark/80 p-2 my-2 rounded-lg shadow-xl relative"
                 >
-                  <button
-                    onClick={() => deleteTask(task._id)} // Llamar la función para eliminar
-                    className="absolute top-2 right-2 hover:text-white/70 text-white/60 px-2.5 py-1.5 rounded-md transition"
-                  >
-                    <TbTrash />
-                  </button>
-                  <h3 className="text-xl font-semibold text-white/85">
-                    {task.title}
-                  </h3>
+                  <div className="flex justify-between">
+                    <h3 className="text-xl font-semibold text-white/85">
+                      {task.title}
+                    </h3>
+                    <div className="flex">
+                      <button
+                        onClick={() => editTask(task._id)}
+                        className="hover:text-white/70 text-white/60 px-2.5 py-1.5 rounded-md transition"
+                      >
+                        <BiEdit />
+                      </button>
+                      <button
+                        onClick={() => deleteTask(task._id)}
+                        className="hover:text-white/70 text-white/60 px-2.5 py-1.5 rounded-md transition"
+                      >
+                        <TbTrash />
+                      </button>
+                    </div>
+                  </div>
                   <p className="text-white/80">{task.description}</p>
                   <p className="text-white/80">Progreso: {task.progress}</p>
                   <div className="bg-primary-light/10 rounded-full w-full h-2 mt-2">
@@ -194,7 +231,7 @@ function TaskUserPage() {
                         width: `${Math.max(
                           0,
                           Math.min(Number(task.progress), 100)
-                        )}%`, // Asegura que el valor esté entre 0 y 100
+                        )}%`,
                       }}
                     />
                   </div>
