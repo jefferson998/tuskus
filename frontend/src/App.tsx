77af188dbc9af83bf9d5c2c54b548866667428df
jefferson998 <pazjefferson11216@gmail.com>
2024-09-30 20:06:08 -0500

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignIn from "./components/sign-in.tsx"
 import RegisterPage from './components/register.tsx';
