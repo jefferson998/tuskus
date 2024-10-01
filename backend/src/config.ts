1d014169fe54a7c792e988437545b8da2eb564a1
jefferson998 <pazjefferson11216@gmail.com>
2024-10-01 10:23:33 -0500

    test

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -4,4 +4,4 @@ export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://fitkro:lQ87dPzG6DCWUbwX@tuskusapi.dahkp.mongodb.net/tuskus?retryWrites=true&w=majority&appName=tuskusapi";
 export const TOKEN_SECRET = process.env.JWT_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://tuskus-frontend-333kay14s-jefferson998s-projects.vercel.app";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://tuskus-frontend.vercel.app";
