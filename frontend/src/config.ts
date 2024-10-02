3bad3bf7777592cad065a52b7cd50a16d8dc618d
jefferson998 <pazjefferson11216@gmail.com>
2024-10-01 21:00:42 -0500

    test last error

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const URL_API = process.env?.URL_API
+export const URL_API = process.env.URL_API
 
-export const URL_HOST = process.env?.URL_HOST
+export const URL_HOST = process.env.URL_HOST
 
 export const EMAIL_TEST ="test11@gmail.com"
 export const PASSWORD_TEST = "test11"
